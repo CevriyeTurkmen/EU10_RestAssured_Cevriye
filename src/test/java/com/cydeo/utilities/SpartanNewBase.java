736721692479e898589455792906329e62cd2fa4
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-04-20 10:43:33 +0100

    Changes

---
+++ src/test/java/com/cydeo/utilities/SpartanNewBase.java
@@ -0,0 +1,58 @@
+package com.cydeo.utilities;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+
+public class SpartanNewBase {
+
+    public static RequestSpecification requestSpec;
+    public static ResponseSpecification responseSpec;
+    public static RequestSpecification userSpec;
+    public static RequestSpecification adminSpec;
+
+
+    @BeforeAll
+    public static void init() {
+
+        baseURI="http://107.23.106.127";
+        port=7000;
+        basePath="/api";
+
+        requestSpec = given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin", "admin")
+                .log().all();
+
+        userSpec =given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("user", "user")
+                .log().all();
+
+        responseSpec = expect().statusCode(200)
+                .and()
+                .contentType(ContentType.JSON)
+                .logDetail(LogDetail.ALL);  //logging with response specficiation
+
+
+
+
+    }
+
+    @AfterAll
+    public static void close(){
+
+       reset();
+    }
+
+}
